12df767b8c717f9430425598644cbfb8dfe14282
paradigm creatives <paradigm@paradigms-Mac-mini.local>
2014-10-06 16:38:13 +0530

    Initial Commit

---
+++ ExamCoreData/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ExamCoreData
+//
+//  Created by paradigm creatives on 10/6/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
